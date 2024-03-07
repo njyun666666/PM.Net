8f23437d2f670c34a6708562dfee77ba040e0af9
Jyun <njyun666666@gmail.com>
2024-03-07 21:29:00 +0800

    add AuthMenus

--- PM.API/Models/Menu/MenuViewModel.cs
+++ PM.API/Models/Menu/MenuViewModel.cs
@@ -17,4 +17,17 @@ namespace PMAPI.Models.Menu
 		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 		public List<MenuViewModel>? Children { get; set; } = null;
 	}
+
+	public class AuthMenuViewModel
+	{
+		public string MenuId { get; set; } = null!;
+		public string? ParentMenuId { get; set; }
+		public string MenuName { get; set; } = null!;
+		public string? Icon { get; set; }
+		public string? Url { get; set; }
+		public bool Enable { get; set; }
+		public int Sort { get; set; }
+		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+		public List<AuthMenuViewModel>? Children { get; set; } = null;
+	}
 }
